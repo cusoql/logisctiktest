47d9f52e60ea4b09075719c907edca4497057633
cvsdxkq <zenaursaev12@gmail.com>
2025-10-27 19:43:27 +0500

    Добавлены правки стилей для h1 и адаптивности

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
-import bgImage from "/assets/ship-bg.png"; // Помести твою картинку в папку src/assets и назови ship-bg.png
+
 
 const LandingPage = () => {
   return (
     <div
-      className="landing"
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
+  className="landing"
+  style={{ backgroundImage: "url('/assets/ship-bg.png')" }}
+>
+
       <div className="overlay"></div>
       <div className="content">
         <h1>Umala Logistics</h1>
-        <p>Надёжные решения для логистики и морских перевозок</p>
+        
         <Link to="/home" className="btn-neon">
           Перейти на сайт
         </Link>
